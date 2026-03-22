96ae803878011ff8940251733b35943029c6358d
JDevRohanY <rohanyadavudp10@gmail.com>
2026-03-22 23:51:06 +0530

    Initial Commit

---
+++ src/main/java/com/example/JdevRohan/splitify/SplitifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.JdevRohan.splitify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JdevRohan/splitify/SplitifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JdevRohan.splitify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
